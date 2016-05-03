ae293e2cdfde2ac77a2f25122ae2dfb6973f650d
ISHPREET NANDA <ishu0827@gmail.com>
2016-05-03 16:43:32 -0700

    Added projects 4/3

---
+++ jQuery/Feedster/app.js
@@ -0,0 +1,13 @@
+var main = function() {
+  $('.notification img').click(function() {
+    $('.notification ul').toggle(250);
+  });
+  
+  $('.btn').click(function() {
+    $(this).toggleClass('btn-like');
+  });
+}
+
+$(document).ready(main);
+
+
---
+++ jQuery/Gameboard/script.js
@@ -0,0 +1,17 @@
+
+var main = function() {
+	$('.more-btn').click(function() {
+    $(this).next().toggle('more-menu');
+  });
+  
+  $('.share').click(function() {
+    $(this).next().toggle('.share-menu');
+  });
+  
+  $('.notification').click(function() {
+    $(this).toggleClass('active');
+  });
+};
+
+
+$(document).ready(main);
